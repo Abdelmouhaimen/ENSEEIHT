fd48f82d09d1b9d68946871697e4672300a0be32
Abdelmouhaimen sarhane <68982208+Abdelmouhaimen@users.noreply.github.com>
2024-06-04 10:21:20 +0200

    Add files via upload

---
+++ M1/S8/Application_Web/Front-End-React/src/Quizprof.js
@@ -0,0 +1,65 @@
+
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import './Quizprof.css';
+import Navbar from './components/Navbar';
+
+
+function Quizprof(){
+    const [etudiant, setEtudiant] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`/API/results?id=${id}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setEtudiant(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []); 
+        
+
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+
+    return (
+        <>
+        <Navbar/>
+        <div>
+            <h2> Resultat {}</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>username</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {etudiant.map(etudiant => (
+                        <tr key={etudiant.id}>
+                            <td>{etudiant.student}</td>
+                            <td>{etudiant.score}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+        </>
+    );
+}
+
+export default Quizprof;
