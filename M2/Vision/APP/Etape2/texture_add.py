7c9fb1aacf3b954d61499a5df894dff42b4386a0
Abdelmouhaimen sarhane <68982208+Abdelmouhaimen@users.noreply.github.com>
2025-01-20 22:53:52 +0100

    Add files via upload

---
+++ M2/Vision/APP/Etape2/texture_add.py
@@ -0,0 +1,31 @@
+from noise_generation import map_colored
+import numpy as np
+from drawer import draw_mesh
+from draw_line import draw
+import cv2
+
+width, height, altitude_map, colors = map_colored(500,500)
+#draw_mesh(width, height, altitude_map, colors)
+
+texture_colored = draw(500, 500,colors).reshape(colors.shape, order="F")
+print(texture_colored.shape)
+
+# combine colors and texture_colored where texture_colored is black
+for i in range(len(texture_colored)):
+    if texture_colored[i][0] != 0 or texture_colored[i][1] != 0 or texture_colored[i][2] != 0:
+        colors[i][0] = texture_colored[i][0]/255
+        colors[i][1] = texture_colored[i][1]/255
+        colors[i][2] = texture_colored[i][2]/255
+        
+
+draw_mesh(width, height, altitude_map, colors)
+
+
+
+
+
+
+
+
+
+
